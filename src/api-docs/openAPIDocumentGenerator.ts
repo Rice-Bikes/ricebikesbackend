034c0cd0e050942c53584962cc2ddc4149c46691
Chase Geyer <chasegeyer04@gmail.com>
2025-01-05 05:37:34 -0800

    setting up initial route

--- src/api-docs/openAPIDocumentGenerator.ts
+++ src/api-docs/openAPIDocumentGenerator.ts
@@ -1,10 +1,10 @@
 import { OpenAPIRegistry, OpenApiGeneratorV3 } from "@asteasolutions/zod-to-openapi";
 
 import { healthCheckRegistry } from "@/api/healthCheck/healthCheckRouter";
-import { userRegistry } from "@/api/user/userRouter";
+import { transactionRegistry } from "@/api/transactions/transactionRouter";
 
 export function generateOpenAPIDocument() {
-  const registry = new OpenAPIRegistry([healthCheckRegistry, userRegistry]);
+  const registry = new OpenAPIRegistry([healthCheckRegistry, transactionRegistry]);
   const generator = new OpenApiGeneratorV3(registry.definitions);
 
   return generator.generateDocument({
